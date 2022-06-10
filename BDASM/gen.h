0dbec7aeb35c2090c830ec8e85739315e4ae4bf7
Iizerd <47404005+Iizerd@users.noreply.github.com>
2022-06-10 01:03:41 -0700

    testexe2. :)

--- BDASM/gen.h
+++ BDASM/gen.h
@@ -2,6 +2,7 @@
 
 #include "addr_width.h"
 #include "inst.h"
+#include "dasm.h"
 
 namespace obf
 {
@@ -27,6 +28,58 @@ namespace obf
 			}
 
 			return result;
+		}
+
+
+		// This is for creating an obfuscated jump within the original binary. One that will jump
+		// to the new place where the function is in a "stealthy way"
+		//
+		template<dasm::address_width Addr_width = dasm::address_width::x64>
+		inline dasm::inst_list_t<Addr_width> routine_jump(dasm::inst_routine_t<Addr_width>& routine)
+		{
+
+		}
+
+
+		// These are for replacing access to the data section with gadgets that load the data from an
+		// absolute address. For example one patched in by the IFF loader.
+		//
+		template<dasm::address_width Addr_width = dasm::address_width::x64>
+		inline dasm::inst_list_t<Addr_width> abs_data_lea(dasm::inst_t<Addr_width>& accessing_inst)
+		{
+
+		}
+		template<dasm::address_width Addr_width = dasm::address_width::x64>
+		inline dasm::inst_list_t<Addr_width> abs_data_mov(dasm::inst_t<Addr_width>& accessing_inst)
+		{
+
+		}
+
+		// If a routine is marked with MARKER_ATTRIBUTE_EXECUTED_ONCE, the obfuscator will generate one of
+		// these to delete the function after its run, probably some gadget that replaces each ret inst.
+		// This can be used on the aforementioned routine_jump gadgets
+		//
+		template<dasm::address_width Addr_width = dasm::address_width::x64>
+		inline dasm::inst_list_t<Addr_width> inst_deleter(dasm::inst_list_t<Addr_width>& list_to_delete)
+		{
+			// For now ill just do a simple loop to zero it, maybe do advanced moving in the future
+			// Make it look like data? => mov byte_ptr[rax+rcx], cl ???
+			// 
+			// 
+			//	push rax
+			//	push rcx
+			//	mov ecx,inst_list_size
+			//	lea rax,[rip+off_to_first_inst]
+			// continue_loop:
+			//	mov byte ptr[rax+rcx], 0
+			//	sub ecx,1
+			//	jnz continue_loop
+			//	pop rcx
+			//	pop rax
+			// 
+			//
+
+
 		}
 	}
 }
